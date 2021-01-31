0ebeb5999dae3100bf1f246d7ab0705f8dd0ccd1
HandelSM <hscholze@hotmail.com>
2021-01-30 22:16:33 -0300

    git ignore update, update to the new GameInput

--- Perdido na Porrada III/Assets/Scripts/PlayerController.cs
+++ Perdido na Porrada III/Assets/Scripts/PlayerController.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Bolt;
+using UnityEngine.InputSystem;
 
 public class PlayerController : Bolt.EntityBehaviour<ICustomPlayerState>
 {
 
     public float speed = 10.0f;
     public Rigidbody2D rb;
+
+    InputMaster controls;
+
+    Vector2 move;
+
+    private void Awake()
+    {
+        controls = new InputMaster();
+
+        controls.Gameplay.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+    }
 
     //void start para o bolt
     public override void Attached()
@@ -19,27 +31,25 @@ public class PlayerController : Bolt.EntityBehaviour<ICustomPlayerState>
     public override void SimulateOwner()
     {
         CheckInputs();
-    }
-
-    //    // Start is called before the first frame update
-    //    public PhotonView photonView;
-    //    public Rigidbody2D rb;
-    //    public GameObject PlayerCamera;
-
-    //    private void Awake()
-    //    {
-    //        if (photonView.isMine)
-    //        {
-    //            Debug.Log("eu tenho photon view");
-    //            PlayerCamera.SetActive(true);
-    //        }
-    //    }
+    }    
 
     private void CheckInputs()
     {
-        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
+        //Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
         //rb.apply move * speed * Time.deltaTim
-        transform.position += move * speed * BoltNetwork.FrameDeltaTime;
+        //transform.position += move * speed * BoltNetwork.FrameDeltaTime;
+        Vector2 m = move * speed * BoltNetwork.FrameDeltaTime;
+        transform.Translate(m, Space.World);
+    }
+
+    private void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Gameplay.Disable();
     }
 
     //    // Update is called once per frame
---
+++ Perdido na Porrada III/Assets/gameInput/inputMaster.cs
@@ -0,0 +1,132 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/gameInput/inputMaster.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputMaster : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputMaster()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""inputMaster"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""6957527f-3ed2-4e69-acc6-eddb02e81377"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""79338279-d9ee-43c2-9043-c9ee02c12279"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""cf329009-f72c-49ca-bcca-069cc994d3f7"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Move;
+    public struct GameplayActions
+    {
+        private @InputMaster m_Wrapper;
+        public GameplayActions(@InputMaster wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Gameplay_Move;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    public interface IGameplayActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
