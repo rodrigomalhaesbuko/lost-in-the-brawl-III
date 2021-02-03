4d5cd0b82a6f5d5f1c3bfcba75519c299c2e3c23
rodrigomalhaesbuko <rodrigomalhaes@aluno.puc-rio.br>
2021-02-02 23:00:14 -0300

    arm trap fixed

--- Throw Hands/Assets/Scripts/LimbComponent.cs
+++ Throw Hands/Assets/Scripts/LimbComponent.cs
@@ -33,7 +33,7 @@ public class LimbComponent : Bolt.EntityBehaviour<ILimbState>
             gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
             gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
             gameObject.GetComponent<Rigidbody2D>().drag = 10f;
-            gameObject.GetComponent<LimbHitComponent>().Damaging = false;
+            limbHitbox.GetComponent<LimbHitComponent>().Damaging = false;
         }
     }
 
