d32872a5c99bd0e1deefb6e938c2acea1aeca276
rodrigomalhaesbuko <rodrigomalhaes@aluno.puc-rio.br>
2021-02-04 17:02:46 -0300

    added timer script

---
+++ Throw Hands/Assets/Scripts/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 99;
+    public bool timerIsRunning = false;
+    public Text timeText;
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                // Ver quem tem mais vida e acabar
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay -= 1;
+
+        timeText.text = timeToDisplay.ToString();
+    }
+}
