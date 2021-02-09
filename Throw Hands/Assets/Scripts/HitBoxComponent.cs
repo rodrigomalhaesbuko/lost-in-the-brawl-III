f710a35f18f3697bf382375035985de92c80f35e
rodrigomalhaesbuko <rodrigomalhaes@aluno.puc-rio.br>
2021-02-09 18:57:43 -0300

    making push when attacked

--- Throw Hands/Assets/Scripts/HitBoxComponent.cs
+++ Throw Hands/Assets/Scripts/HitBoxComponent.cs
@@ -8,7 +8,19 @@ public class HitBoxComponent : Bolt.EntityBehaviour<ICustomPlayerState>
     public PlayerStatus player;
     public PlayerType myLimbs;
     public Rigidbody2D myRgbody;
-    public float impactForce = 30f;
+    public float impactForce = 30.0f;
+
+    private void Start()
+    {
+       
+        if (BoltNetwork.IsClient)
+        {
+            impactForce = 30f;
+        }else
+        {
+            impactForce = -30f;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -20,14 +32,14 @@ public class HitBoxComponent : Bolt.EntityBehaviour<ICustomPlayerState>
 
                 if(collision.gameObject.GetComponent<LimbHitComponent>().rdbody.velocity.x > 0.0f)
                 {
-                    myRgbody.AddForce(Vector2.right * impactForce);
+                    myRgbody.AddForce(new Vector2(-impactForce, 0f), ForceMode2D.Impulse);
                 }
                 else
                 {
-                    myRgbody.AddForce(Vector2.left * impactForce);
+                    myRgbody.AddForce(new Vector2(impactForce, 0f), ForceMode2D.Impulse);
                 }
                 collision.gameObject.GetComponent<LimbHitComponent>().rdbody.velocity = Vector2.zero;
-                collision.gameObject.GetComponent<LimbHitComponent>().rdbody.AddForce(Vector2.down * 3.0f);
+                collision.gameObject.GetComponent<LimbHitComponent>().rdbody.AddForce(new Vector2(0f, -10.0f),ForceMode2D.Impulse);
 
                 collision.gameObject.GetComponent<LimbHitComponent>().myColider.isTrigger = true;
                 player.TakeDamage();
