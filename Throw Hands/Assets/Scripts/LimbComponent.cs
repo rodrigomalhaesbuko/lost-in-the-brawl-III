71fd4af9e6854771dfb15dbd6c88dcc9cf8e2db4
Giovanni Severo <giosvro@gmail.com>
2021-02-10 16:00:28 -0800

    Testa remoção de fisica

--- Throw Hands/Assets/Scripts/LimbComponent.cs
+++ Throw Hands/Assets/Scripts/LimbComponent.cs
@@ -31,8 +31,8 @@ public class LimbComponent : Bolt.EntityBehaviour<ILimbState>
         if (IsGrounded())
         {
             gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
-            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
-            gameObject.GetComponent<Rigidbody2D>().drag = 10f;
+            //gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+            //gameObject.GetComponent<Rigidbody2D>().drag = 10f;
             limbHitbox.GetComponent<LimbHitComponent>().Damaging = false;
         }
     }
