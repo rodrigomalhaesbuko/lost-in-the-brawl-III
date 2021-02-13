d467913cd627a3a210d130c5730592243bff028e
rodrigomalhaesbuko <rodrigomalhaes@aluno.puc-rio.br>
2021-02-13 06:54:05 -0300

    test bug

--- Throw Hands/Assets/Scripts/HitBoxComponent.cs
+++ Throw Hands/Assets/Scripts/HitBoxComponent.cs
@@ -30,24 +30,24 @@ public class HitBoxComponent : Bolt.EntityBehaviour<ICustomPlayerState>
             if(collision.gameObject.GetComponent<LimbHitComponent>().LimbComponent.playerType != myLimbs && collision.gameObject.GetComponent<LimbHitComponent>().Damaging )
             {
 
-                if (playerObject.GetComponent<PlayerStatus>().isFlipped)
-                {
-                    impactForce *= -1;
-                }
+                //if (playerObject.GetComponent<PlayerStatus>().isFlipped)
+                //{
+                //    impactForce *= -1;
+                //}
 
-                if (state.Health == 1 || state.EnemyHealth == 1) //tem q mudar, checar se o cara vai tomar o last hit
-                {
-                    impactForce *= 1.5f;
-                }
+                //if (state.Health == 1 || state.EnemyHealth == 1) //tem q mudar, checar se o cara vai tomar o last hit
+                //{
+                //    impactForce *= 1.5f;
+                //}
 
-                if (collision.gameObject.GetComponent<LimbHitComponent>().rdbody.velocity.x > 0.0f)
-                {
-                    myRgbody.AddForce(new Vector2(-impactForce, 0f), ForceMode2D.Impulse);
-                }
-                else
-                {
-                    myRgbody.AddForce(new Vector2(impactForce, 0f), ForceMode2D.Impulse);
-                }
+                //if (collision.gameObject.GetComponent<LimbHitComponent>().rdbody.velocity.x > 0.0f)
+                //{
+                //    myRgbody.AddForce(new Vector2(-impactForce, 0f), ForceMode2D.Impulse);
+                //}
+                //else
+                //{
+                //    myRgbody.AddForce(new Vector2(impactForce, 0f), ForceMode2D.Impulse);
+                //}
 
                 collision.gameObject.GetComponent<LimbHitComponent>().rdbody.velocity = Vector2.zero;
                 collision.gameObject.GetComponent<LimbHitComponent>().rdbody.AddForce(new Vector2(0f, -5.0f),ForceMode2D.Impulse);
