b15ee4ee6bc84411acede54cb351f60888bd3c23
rodrigomalhaesbuko <rodrigomalhaes@aluno.puc-rio.br>
2021-03-02 21:55:28 -0300

    working aroud local mode

---
+++ Throw Hands/Assets/Scripts/LocalPlay/LocalGameManager.cs
@@ -0,0 +1,357 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
+
+public class LocalGameManager : MonoBehaviour
+{
+    public GameState gameState;
+
+    public GameObject counter;
+    public GameObject roomName;
+
+    public GameObject playerPrefab;
+    public GameObject playerPrefab2;
+
+    public GameObject lifeHost;
+    public GameObject lifeClient;
+
+    public GameObject DouglasInstance;
+    public GameObject CarlousInstance;
+
+    public GameObject WaitingPlayer;
+
+    private bool gameStarted = false;
+
+    public float battleOffset = 5f;
+
+    public AudioSource bgm;
+
+    //CAMERA
+    public CinemachineTargetGroup targetGroup;
+
+    //endbattle vars
+    public GameObject hostWins;
+    public GameObject clientWins;
+    private int hostScore = 0;
+    private int clientScore = 0;
+
+    public GameObject youwin;
+    public GameObject youlose;
+    public GameObject youDraw;
+
+    public GameObject ThrowHands;
+
+    public GameObject seta;
+    public GameObject RematchBox;
+    public GameObject healthBar;
+    private const float stepSeta = 580;
+    private const int setavel = 100;
+    private InputMaster controls;
+    private int setapos;
+    private float posxseta = 0;
+    private int dirseta = 1;
+    private bool p1AcceptRematch = false;
+    private bool p2AcceptRematch = false;
+
+    public static bool armFlip = false;
+
+    public float slowSeconds = 4.0f;
+
+    private float slowTimer = 0f;
+
+
+    private void Awake()
+    {
+        controls = new InputMaster();
+
+        controls.StaticScene.Move.performed += ctx =>
+        {
+            if (gameState == GameState.rematch)
+            {
+                Vector2 movesetadir = ctx.ReadValue<Vector2>();
+
+                if (movesetadir.x == 1)
+                {
+                    setapos = 1;
+                }
+                else if (movesetadir.x == -1)
+                {
+                    setapos = 0;
+                }
+            }
+        };
+
+        controls.StaticScene.Select.performed += _ =>
+        {
+            if (gameState == GameState.rematch)
+            {
+                gameState = GameState.rematchClick;
+
+                if (setapos == 0)
+                {
+                    RestartGame();
+                }
+                else if (setapos == 1)
+                {
+                    SceneManager.LoadScene("SampleScene");
+                }
+            }
+        };
+    }
+
+    void CreateGame()
+    {
+        gameState = GameState.intro;
+
+        audioControl.PlaySound(SFXType.Intro);
+
+        if (GameObject.FindGameObjectWithTag("musicMenu") != null)
+        {
+            GameObject.FindGameObjectWithTag("musicMenu").GetComponent<AudioSource>().Stop();
+        }
+
+        //Player 1
+        playerPrefab.GetComponent<PlayerStatus>().lifeHost = lifeHost;
+        playerPrefab.GetComponent<PlayerStatus>().lifeClient = lifeClient;
+        playerPrefab.GetComponent<PlayerStatus>().GameController = gameObject;
+        playerPrefab.GetComponent<PlayerStatus>().playerType = PlayerType.Douglas;
+
+        //Player 2
+        playerPrefab2.GetComponent<PlayerStatus>().lifeHost = lifeHost;
+        playerPrefab2.GetComponent<PlayerStatus>().lifeClient = lifeClient;
+        playerPrefab2.GetComponent<PlayerStatus>().GameController = gameObject;
+        playerPrefab2.GetComponent<PlayerStatus>().playerType = PlayerType.Carlous;
+
+        DouglasInstance = BoltNetwork.Instantiate(playerPrefab, new Vector2(
+                this.transform.position.x + (battleOffset * -1.5f), this.transform.position.y), Quaternion.identity);
+
+        CarlousInstance = BoltNetwork.Instantiate(playerPrefab2, new Vector2(
+                this.transform.position.x + (battleOffset * 1.5f), this.transform.position.y), Quaternion.identity);
+
+
+        targetGroup.AddMember(DouglasInstance.transform, 1f, 4f);
+        targetGroup.AddMember(CarlousInstance.transform, 1f, 4f);
+
+        WaitingPlayer.SetActive(false);
+        bgm.Play();
+        StartCoroutine(WaitCreateGame());
+    }
+
+    private IEnumerator WaitCreateGame()
+    {
+        ThrowHands.SetActive(true);
+        ThrowHands.GetComponent<Animator>().Play("ThrowHandsCutIn");
+        yield return new WaitForSeconds(1.82f);
+        counter.GetComponent<Timer>().timerIsRunning = true;
+        gameState = GameState.play;
+        DouglasInstance.GetComponent<PlayerController>().enableControls();
+        CarlousInstance.GetComponent<PlayerController>().enableControls();
+        ThrowHands.SetActive(false);
+    }
+
+
+    private void RestartGame()
+    {
+        GameObject[] limbs = GameObject.FindGameObjectsWithTag("limb");
+
+        foreach (GameObject limb in limbs)
+        {
+            Destroy(limb);
+        }
+
+
+        if (DouglasInstance != null)
+            Destroy(DouglasInstance);
+   
+        if (CarlousInstance != null)
+            Destroy(CarlousInstance);
+    
+        gameState = GameState.restart;
+        slowTimer = 0f;
+        Time.timeScale = 1f;
+        youwin.SetActive(false);
+        youlose.SetActive(false);
+        youDraw.SetActive(false);
+        p1AcceptRematch = false;
+        p2AcceptRematch = false;
+        RematchBox.SetActive(false);
+        lifeHost.SetActive(true);
+        lifeClient.SetActive(true);
+        healthBar.SetActive(true);
+        counter.SetActive(true);
+        counter.GetComponent<Timer>().timeRemaining = PlayerPrefs.GetFloat("gameDuration");
+
+        audioControl.audioSource.Stop();
+        controls.StaticScene.Disable();
+        Time.timeScale = 1f;
+
+        CreateGame();
+    }
+
+
+    private void Flip()
+    {
+        Vector3 newScaleDouglas = DouglasInstance.transform.localScale;
+        newScaleDouglas.x *= -1;
+        DouglasInstance.transform.localScale = newScaleDouglas;
+        DouglasInstance.GetComponent<PlayerStatus>().isFlipped = !DouglasInstance.GetComponent<PlayerStatus>().isFlipped;
+
+        Vector3 newScaleCarlous = CarlousInstance.transform.localScale;
+        newScaleCarlous.x *= -1;
+        CarlousInstance.transform.localScale = newScaleCarlous;
+        CarlousInstance.GetComponent<PlayerStatus>().isFlipped = !CarlousInstance.GetComponent<PlayerStatus>().isFlipped;
+
+        //// jump
+        //if (armFlip)
+        //{
+
+        //    CarlousInstance.GetComponent<Rigidbody2D>().AddForce(new Vector2( -5f , 0f), ForceMode2D.Impulse);
+        //    //DouglasInstance.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10f), ForceMode2D.Impulse);
+        //}
+        //else
+        //{
+        //    //CarlousInstance.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10f), ForceMode2D.Impulse);
+        //    DouglasInstance.GetComponent<Rigidbody2D>().AddForce(new Vector2(5f, 0f), ForceMode2D.Impulse);
+        //}
+
+
+        armFlip = !armFlip;
+    }
+
+    private void Update()
+    {
+        if (gameState == GameState.rematch)
+        {
+            posxseta += setavel * Time.deltaTime * dirseta;
+
+            if (posxseta > 30)
+            {
+                dirseta = -1;
+            }
+            else if (posxseta < 0)
+            {
+                dirseta = 1;
+            }
+
+            seta.GetComponent<RectTransform>().localPosition = new Vector3(-443 + posxseta + setapos * stepSeta, -440, 0);
+        }
+
+        if (gameState == GameState.play)
+        {
+
+            if (DouglasInstance.transform.position.x > CarlousInstance.transform.position.x)
+            {
+                if (!CarlousInstance.GetComponent<PlayerStatus>().isFlipped && !DouglasInstance.GetComponent<PlayerStatus>().isFlipped)
+                {
+                    Flip();
+                }
+            }
+
+            if (CarlousInstance.transform.position.x > DouglasInstance.transform.position.x)
+            {
+                if (CarlousInstance.GetComponent<PlayerStatus>().isFlipped && DouglasInstance.GetComponent<PlayerStatus>().isFlipped)
+                {
+                    Flip();
+                }
+            }
+
+        }
+
+        if (gameState == GameState.slowed)
+        {
+            Time.timeScale = .05f;
+
+            slowTimer += Time.unscaledDeltaTime;
+
+            if (slowTimer > slowSeconds)
+            {
+                Time.timeScale = 1.0f;
+            }
+
+        }
+
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+        audioControl.audioSource.pitch = Time.timeScale;
+    }
+
+    public void CheckDraw()
+    {
+        if (DouglasInstance.GetComponent<PlayerStatus>().state.Health > CarlousInstance.GetComponent<PlayerStatus>().state.EnemyHealth)
+        {
+            endGame(true, false);
+        }
+        else if (DouglasInstance.GetComponent<PlayerStatus>().state.Health < CarlousInstance.GetComponent<PlayerStatus>().state.EnemyHealth)
+        {
+            endGame(false, false);
+        }
+        else
+        {
+            endGame(false, true);
+        }
+    }
+
+    private void MakeSlowMotion()
+    {
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+    }
+
+    private IEnumerator afterEndGame(bool hostWon, bool draw)
+    {
+        MakeSlowMotion();
+
+        if (DouglasInstance != null)
+        {
+            DouglasInstance.GetComponent<PlayerController>().disableControls();
+        }
+
+        if (CarlousInstance != null)
+        {
+            CarlousInstance.GetComponent<PlayerController>().disableControls();
+        }
+
+        yield return new WaitForSeconds(1.2f);
+
+        audioControl.audioSource.Stop();
+        audioControl.PlaySound(SFXType.End);
+
+        if (draw)
+        {
+            youDraw.SetActive(true);
+            audioControl.PlaySound(SFXType.Draw);
+        }
+
+        if (hostWon)
+            hostScore++;
+        else
+            clientScore++;
+
+        hostWins.GetComponent<Text>().text = hostScore.ToString();
+        clientWins.GetComponent<Text>().text = clientScore.ToString();
+
+        RematchBox.SetActive(true);
+        lifeHost.SetActive(false);
+        lifeClient.SetActive(false);
+        healthBar.SetActive(false);
+        counter.SetActive(false);
+        counter.GetComponent<Timer>().timerIsRunning = false;
+
+        controls.StaticScene.Enable();
+
+        gameState = GameState.rematch;
+
+    }
+
+    public void endGame(bool hostWon, bool draw)
+    {
+        if (gameState != GameState.slowed)
+        {
+            gameState = GameState.slowed;
+            StartCoroutine(afterEndGame(hostWon, draw));
+        }
+    }
+
+}
---
+++ Throw Hands/Assets/Scripts/LocalPlay/LocalPlayerController.cs
@@ -0,0 +1,226 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalPlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public Rigidbody2D rb;
+    public float jumpForce;
+    public float colliderRaySize;
+    public Collider2D groundCollider;
+    public Animator playerAnimator;
+    public GameObject shoes;
+    private bool parrying;
+    private bool enableParry = true;
+    private bool enableParryAnimation = true;
+    private bool alreadyJumped = false;
+
+    public bool isDucking = false;
+
+    public InputMaster controls;
+
+    Vector2 move;
+
+    private void Awake()
+    {
+        controls = new InputMaster();
+
+        controls.Gameplay.Move.performed += ctx =>
+        {
+            move = ctx.ReadValue<Vector2>();
+        };
+
+        controls.Gameplay.Parry.started += ctx =>
+        {
+            if (enableParry)
+            {
+                enableParry = false;
+                parrying = true;
+                //StartCoroutine(PrepareParry());   
+            }
+        };
+
+        controls.Gameplay.Parry.canceled += ctx => { enableParry = true; };
+    }
+
+    private void Start()
+    {
+        playerAnimator = gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>();
+    }
+
+    //private IEnumerator PrepareParry()
+    //{
+    //    yield return new WaitForEndOfFrame();
+    //    parrying = false;
+    //}
+
+    public void Update()
+    {
+        if (move.x == 0.0f)
+        {
+            Animator.SetBool("IsWalking", false);
+            //Debug.Log("Ta andando");
+        }
+        else
+        {
+            state.Animator.SetBool("IsWalking", true);
+            //Debug.Log("Nao Ta andando");
+        }
+
+        if (IsGrounded())
+        {
+            if (state.MoveY >= 0.5f)
+            {
+                if (!alreadyJumped)
+                {
+                    alreadyJumped = true;
+                    state.Animator.SetBool("Jump", true);
+                    Jump();
+                }
+            }
+            else
+            {
+                state.Animator.SetBool("Jump", false);
+                alreadyJumped = false;
+            }
+        }
+
+        if (state.MoveY < -0.5f)
+        {
+            isDucking = true;
+            state.Animator.SetBool("Duck", true);
+        }
+        else
+        {
+            isDucking = false;
+            state.Animator.SetBool("Duck", false);
+        }
+
+        if (state.Parrying)
+        {
+            if (enableParryAnimation)
+            {
+                if ((state.LeftArmEnable || state.RightArmEnable) && !isDucking)
+                {
+                    enableParryAnimation = false;
+                    state.Animator.SetTrigger("ParryT");
+                    StartCoroutine(ParryAnimation());
+                }
+                else
+                {
+                    parrying = false;
+                }
+            }
+            else
+            {
+                parrying = false;
+            }
+        }
+
+
+
+        //if(state.MyX > state.OtherX)
+        //{
+        //        Vector3 newScale = gameObject.transform.localScale;
+        //        newScale.x *= -1;
+        //        gameObject.transform.localScale = newScale;
+
+        //}else if (state.MyX < state.OtherX)
+        //{
+        //        Vector3 newScale = gameObject.transform.localScale;
+        //        newScale.x *= -1;
+        //        gameObject.transform.localScale = newScale;
+        //}
+    }
+
+    private IEnumerator ParryAnimation()
+    {
+        StartCoroutine(Block(gameObject.GetComponent<LimbShooter>().parryAnimationTime));
+        yield return new WaitForSeconds(gameObject.GetComponent<LimbShooter>().parryAnimationTime);
+        enableParry = true;
+        parrying = false;
+        enableParryAnimation = true;
+    }
+
+    private IEnumerator Block(float animationTime)
+    {
+        gameObject.GetComponent<LimbShooter>().HitBox.SetActive(false);
+        gameObject.GetComponent<LimbShooter>().Block.SetActive(true);
+        yield return new WaitForSeconds(animationTime / 2);
+        gameObject.GetComponent<LimbShooter>().HitBox.SetActive(true);
+        gameObject.GetComponent<LimbShooter>().Block.SetActive(false);
+    }
+
+
+    private void CheckInputs()
+    {
+        //Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
+        ////rb.apply move * speed * Time.deltaTim
+        //transform.position += move * speed * BoltNetwork.FrameDeltaTime;
+        Vector2 m = move * speed * BoltNetwork.FrameDeltaTime;
+        state.MoveX = move.x;
+        state.MoveY = move.y;
+        state.Parrying = parrying;
+
+        //Debug.Log(BoltMatchmaking.CurrentSession.ConnectionsCurrent);
+
+        if (!isDucking)
+        {
+            transform.Translate(new Vector2(m.x, 0f), Space.World);
+        }
+
+
+        //transform.position += new Vector3(m.x, 0f, transform.position.z);
+
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    }
+
+    public float bola = 10f;
+
+    public bool IsGrounded()
+    {
+        if (shoes.GetComponent<ShoeComponent>().onFloor)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void disableControls()
+    {
+        move = Vector2.zero;
+        controls.Gameplay.Disable();
+    }
+
+    public void enableControls()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    //private void OnEnable()
+    //{
+    //    controls.Gameplay.Enable();
+    //}
+
+    private void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
+
+    //    // Update is called once per frame
+    //    void Update()
+    //    {
+    //        if (photonView.isMine)
+    //        {
+    //            CheckInputs();
+    //        }
+    //    }
+}
